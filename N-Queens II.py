6545f225c89357bcd9c936136abd0a46e5cbb777
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-05 22:29:46 -0700

    Add files via upload

---
+++ N-Queens II.py
@@ -0,0 +1,30 @@
+# 52. N-Queens II
+
+# The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+
+# Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+        res = 0
+        
+        def backtrack(r):
+            if r == n:
+                nonlocal res
+                res += 1
+                return
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                backtrack(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+        backtrack(0)
+        return res
