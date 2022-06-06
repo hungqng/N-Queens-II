a1f2e5c0e98d6b372e30b52d39e1c682e2b9099e
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-05 22:30:29 -0700

    Add files via upload

--- N-Queens II.py
+++ N-Queens II.py
@@ -28,3 +28,16 @@ class Solution:
                 negDiag.remove(r - c)
         backtrack(0)
         return res
+
+        # Solution 2
+        def DFS(queens, xy_dif, xy_sum):
+            p = len(queens)
+            if p==n:
+                result.append(queens)
+                return None
+            for q in range(n):
+                if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
+                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])  
+        result = []
+        DFS([],[],[])
+        return len([ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result])
